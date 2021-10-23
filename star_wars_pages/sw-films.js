edbb535f475f2b5549979512009429d7c66c8afd
Teison Edwards <teison.edwards@gmail.com>
2021-10-23 17:19:15 -0600

    Copied posters page start.

--- star_wars_pages/sw-films.js
+++ star_wars_pages/sw-films.js
@@ -1,9 +1,17 @@
-import {films} from '../data/films.js'
+import {films} from './data/films.js'
 
-let filmList = document.querySelector('filmList')
+let filmList = document.querySelector('#filmList')
 
-let figure = document.createElement('figure')
+for (let i = 0; i <films.length; i++){
+    let figure = document.createElement{'figure'}
+    let figImg = document.createElement{'img'}
+    figImg.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
+    let figCaption = document.createElement{'figcaption'}
+    figCaption.textContent = 'File Title Goes Here'
 
+    figure.appendChild{figImg}
+    figure.appendChild{figCaption}
+    figure.appendChild{figure}
+}
 // add a function later that will ge the url propery and use array.find to match the
 // titles with the films
-
