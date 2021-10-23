ee99b518b45133aa4cb18c006ed9ee05f0c3bc27
Teison Edwards <teison.edwards@gmail.com>
2021-10-23 17:58:43 -0600

    Started Following along Oct 14th video

--- star_wars_pages/sw_films/sw_films.js
+++ star_wars_pages/sw_films/sw_films.js
@@ -1,19 +1,26 @@
 import { films } from '../data/films.js'
 
-let filmList = document.querySelector('#filmList')
+console.log("hello! This is in the console")
 
-filmList.textContent = 'This is my content, it isnt very long.'
+I left off on the Oct 14th recording 13min in
 
-for (let i = 0; i <films.length; i++){
-    let figure = document.createElement{'figure'}
-    let figImg = document.createElement{'img'}
-    figImg.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
-    let figCaption = document.createElement{'figcaption'}
-    figCaption.textContent = 'File Title Goes Here'
+// let filmList = document.querySelector('#filmList')
 
-    figure.appendChild{figImg}
-    figure.appendChild{figCaption}
-    figure.appendChild{figure}
-}
+// filmList.textContent = 'This is my content, it isnt very long.'
+
+// for (let i = 0; i <films.length; i++){
+//     let figure = document.createElement{'figure'}
+//     let figImg = document.createElement{'img'}
+//     figImg.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
+//     let figCaption = document.createElement{'figcaption'}
+//     figCaption.textContent = 'File Title Goes Here'
+
+//     figure.appendChild{figImg}
+//     figure.appendChild{figCaption}
+//     figure.appendChild{figure}
+// }
+
 // add a function later that will ge the url propery and use array.find to match the
 // titles with the films
+
+//If you select a block of code and use the key sequence Ctrl+K+C, you'll comment out the section of code. Ctrl+K+U will uncomment the code. 
