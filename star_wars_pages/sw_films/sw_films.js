772a1445024b8b607cc84bd617d48f7266cdb359
Teison Edwards <teison.edwards@gmail.com>
2021-10-26 08:01:06 -0600

    created a for loop for the posters' figures

--- star_wars_pages/sw_films/sw_films.js
+++ star_wars_pages/sw_films/sw_films.js
@@ -2,19 +2,17 @@ import { films } from '../data/films.js'
 
 let filmList= document.querySelector('#filmList')
 
-filmList.textContent = 'this is my content'
-
-let myImg = document.createElement('img')
-myImg.src = `https://starwars-visualguide.com/assets/img/films/7.jpg`
-
-filmList.appendChild(myImg)
-
-let titleList = document.createElement('ol')
-filmList.appendChild(titleList)
-
 for (let i = 0; i<films.length; i++){
-    console.log(films[i].title)
-    let listItem = document.createElement('li')
-    listItem.textContent = films[i].title
-    titleList.appendChild(listItem)
+    let figure = document.createElement('figure')
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/7.jpg`
+    let figCaption = document.createElement('figcaption')
+    figCaption.textContent = "Film Title Goes here"
+
+figure.appendChild(figImg)
+figure.appendChild(figCaption)
+filmList.appendChild(figure)
 }
+
+//let titleList = document.createElement('ol')
+//filmList.appendChild(titleList)
