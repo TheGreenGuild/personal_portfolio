6e83a0868d4a188f8031569c0407f4e93f3248dc
Teison Edwards <teison.edwards@gmail.com>
2021-10-26 09:42:25 -0600

    Done with films for now

--- star_wars_pages/sw_films/sw_films.js
+++ star_wars_pages/sw_films/sw_films.js
@@ -5,9 +5,11 @@ let filmList= document.querySelector('#filmList')
 for (let i = 0; i<films.length; i++){
     let figure = document.createElement('figure')
     let figImg = document.createElement('img')
-    figImg.src = `https://starwars-visualguide.com/assets/img/films/7.jpg`
+    figImg.src = `https://starwars-visualguide.com/assets/img/films/${i+1}.jpg`
     let figCaption = document.createElement('figcaption')
-    figCaption.textContent = "Film Title Goes here"
+
+    figCaption.textContent = `https://swapi.co/api/films/5/`
+    //add a function later with array.find to match titles with posters
 
 figure.appendChild(figImg)
 figure.appendChild(figCaption)
