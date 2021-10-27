35133d50a99301746814fea9740815a048e58907
Teison Edwards <teison.edwards@gmail.com>
2021-10-27 10:57:51 -0600

    Mostly done with char page. Buttons don't work yet

--- star_wars_pages/sw_characters/sw_characters.js
+++ star_wars_pages/sw_characters/sw_characters.js
@@ -1,14 +1,35 @@
 import { people } from '../data/people.js'
 
+const mainBody = document.querySelector("body")
+const header = document.createElement("header")
+const maleButton = document.createElement("button")
+maleButton.textContent = "Male Characters"
+const femaleButton = document.createElement("button")
+femaleButton.textContent = "Female Characters"
+header.appendChild(maleButton)
+header.appendChild(femaleButton)
+
 const mainContent = document.querySelector("#main")
 
-people.forEach(element =>{
+document.body.insertBefore(header, mainContent)
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+const femaleCharacters = people.filter(person => person.gender === 'female')
+
+
+
+people.forEach((person, index) =>{
     const charFigure = document.createElement('figure')
     const charImg = document.createElement('img')
-    charImg.src =`https://starwars-visualguide.com/assets/img/characters/1.jpg`
-    charFigure.appendChild(charImg)
-    mainContent.appendChild(charFigure)
+    charImg.src =`https://starwars-visualguide.com/assets/img/characters/${index+1}.jpg`
     
+
+    const charCaption = document.createElement('figcaption')
+    charCaption.textContent = person.name
+    
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+    mainContent.appendChild(charFigure)
 })
 
 
