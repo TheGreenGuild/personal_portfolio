9967f5ab6966b9ee52b366bf06cc529a2dcea68f
Teison Edwards <teison.edwards@gmail.com>
2021-10-28 11:22:01 -0600

    Started star wars ships page

---
+++ star_wars_pages/star_ships/sw_starships.js
@@ -0,0 +1,30 @@
+import { starships } from "../data/starships.js"
+
+//Start of Nav stuff
+const nav = document.querySelector(".nav")
+const navList = document.querySelector(".navList")
+const shipView = document.querySelector(".displaySection")
+
+function populateNav(starships) {
+    starships.forEach(starship => {
+        let anchorWrap = document.createElement("a")
+        anchorWrap.href = "#"
+        let listItem = document.createElement("li")
+        listItem.textContent = starship.name
+        anchorWrap.addEventListener('click', () => {
+            populateShipView(starship)
+        })
+
+
+        anchorWrap.appendChild(listItem)
+        navList.appendChild(anchorWrap)
+    })
+}
+populateNav(starships)
+
+function populateShipView(shipData) {
+    console.log(`thanks for cliking on ${shipData.name}`)
+    let shipImage = document.createElement("img")
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/5.jpg`
+    shipView.appendChild(shipImage)
+}
