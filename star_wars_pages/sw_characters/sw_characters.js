1da493e773a9ddacb51d45af40f732fcc6118f9c
Teison Edwards <teison.edwards@gmail.com>
2021-10-28 10:06:10 -0600

    Got buttons working

--- star_wars_pages/sw_characters/sw_characters.js
+++ star_wars_pages/sw_characters/sw_characters.js
@@ -1,55 +1,57 @@
-import { people } from '../data/people.js'
+import { people } from "../data/people.js";
 
-const mainBody = document.querySelector("body")
-const header = document.createElement("header")
-const maleButton = document.createElement("button")
-maleButton.textContent = "Male Characters"
-const femaleButton = document.createElement("button")
-femaleButton.textContent = "Female Characters"
-header.appendChild(maleButton)
-header.appendChild(femaleButton)
+const mainContent = document.querySelector("#main");
 
-const mainContent = document.querySelector("#main")
+populateDOM(people);
 
-document.body.insertBefore(header, mainContent)
+// IDK what happend to this line of code --->const mainBody = document.querySelector("body")
+const header = document.createElement("header");
+const maleButton = document.createElement("button");
+maleButton.textContent = "Male Characters";
+maleButton.addEventListener("click", () => populateDOM(maleCharacters));
 
-const maleCharacters = people.filter(person => person.gender === 'male')
-const femaleCharacters = people.filter(person => person.gender === 'female')
+const femaleButton = document.createElement("button");
+femaleButton.textContent = "Female Characters";
+header.appendChild(maleButton);
+header.appendChild(femaleButton);
+femaleButton.addEventListener("click", () => populateDOM(femaleCharacters));
 
+document.body.insertBefore(header, mainContent);
 
+const maleCharacters = people.filter((person) => person.gender === "male");
+const femaleCharacters = people.filter((person) => person.gender === "female");
+const otherCharacters = people.filter((person) => {
+  if (person.gender !== "male" && person.gender !== "female") {
+    return person;
+  }
+});
 
-people.forEach((person, index) =>{
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
-    charImg.src =`https://starwars-visualguide.com/assets/img/characters/${index+1}.jpg`
-    
+function populateDOM(characters) {
+  //first clear the page, then populate
+  while (mainContent.firstChild) {
+    //The list is LIVE so it will reindex each call
+    mainContent.removeChild(mainContent.firstChild);
+  }
+  characters.forEach((person, index) => {
+    const charFigure = document.createElement("figure");
+    const charImg = document.createElement("img");
+    const charNum = getLastNumber(person.url);
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
 
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = person.name
-    
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
-    mainContent.appendChild(charFigure)
-})
+    const charCaption = document.createElement("figcaption");
+    charCaption.textContent = person.name;
 
-
-
-//This is all stuff we covered in our Oct 26th class v v v 
-/*
-maleButton.addEventListener('click', () => console.log("Thanks for listening!"))
-
-const charNum = getLastNumber(element.url)
-    charImg.src - `url ${charNum}`
+    charFigure.appendChild(charImg);
+    charFigure.appendChild(charCaption);
+    mainContent.appendChild(charFigure);
+  });
+}
 
 function getLastNumber(url) {
-    let end = url.lastIndexOf('/')
-    let start = end-2
-    if(url.charAt(start) === '/'){
-        start++
-    }
-    return url.slice(start, end)
+  let end = url.lastIndexOf("/");
+  let start = end - 2;
+  if (url.charAt(start) === "/") {
+    start++;
+  }
+  return url.slice(start, end);
 }
-*/
-//in todays class Oct 26th we covered how to get the correct photos using cool string
-//methods. Then we talked about the OS and hoisting. I can skip that stuff later. 
-//Then about  min before class end we talked about 
