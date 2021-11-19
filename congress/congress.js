81d051b1905bc1d18b1f65e04fef6740f8724c05
Teison Edwards <teison.edwards@gmail.com>
2021-11-18 17:52:22 -0700

    Gone as far as Thor will carry us.

--- congress/congress.js
+++ congress/congress.js
@@ -1,10 +1,22 @@
 import {senators} from "../data/senators.js"
+import {representatives} from "../data/representatives.js"
+
+const members = [...senators, ...representatives] //Modern way to combine arrays like a genus! lol 
+
 //We select what we want to attach this stuff to
 const senatorDiv = document.querySelector('.senators')
+const loyaltyHeading = document.querySelector('.mostLoyal')
+const seniorityHeading = document.querySelector('.seniority')
+
+console.log(members)
 
 //We simplify the data
-function simplifiedSenators(){
-    return senators.map(senator =>{
+function simplifiedMembers(chamberFilter) {
+    //this stuff here will do the simplified members on everything unless we pass it a chamber, 
+    //then it will show the one or the other
+    const filteredArray =  members.filter((member) => chamberFilter ? member.short_title === chamberFilter : member,)
+    
+    return filteredArray.map(senator =>{
         let middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `
         return {
             id: senator.id,
@@ -12,14 +24,17 @@ function simplifiedSenators(){
             party: senator.party,
             gender: senator.gender, 
             seniority: +senator.seniority,
-            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+            missedVotesPct: senator.missed_votes_pct,
+            loyaltyPct: senator.votes_with_party_pct,
         }
     })
+
 }
 
 //We assemble the pieces of our figures using our simple data
-function populateSenatorDiv(simplifiedSenators) {
-    simplifiedSenators.forEach(senator =>{
+function populateSenatorDiv(simplifiedMembers) {
+    simplifiedMembers.forEach(senator =>{
         const senFigure = document.createElement('figure')
         const figImg = document.createElement('img')
         const figCaption = document.createElement('figcaption')
@@ -34,14 +49,27 @@ function populateSenatorDiv(simplifiedSenators) {
     
 }
 
+//Got a few fun/useful values
+//const filterSenators = (prop, value) => simplifiedMembers().filter(senator => senator[prop] === value)
+const mostSeniorMember = simplifiedMembers().reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
+const mostLoyal = simplifiedMembers().reduce((acc, senator) => {
+    if (senator.loyaltyPct === 100){
+        acc.push(senator)
+    }
+    return acc
+}, [])
+const cowardList = document.createElement('ol')
+const spineless = mostLoyal.map(coward => {
+    let listItem = document.createElement('li')
+    listItem.textContent = coward.name
+    cowardList.appendChild(listItem)
+})
 
-const filterSenators = (prop, value) => simplifiedSenators().filter(senator => senator[prop] === value)
+//Made the headers say stuff
+seniorityHeading.textContent = `The most senior member of congress is ${mostSeniorMember.name} who has been in congress for ${mostSeniorMember.seniority} years.`
+loyaltyHeading.textContent = `The most spinless members of congress who vote with their party 100% of the time are: `
 
-const mostSeniorSenator = simplifiedSenators().reduce((acc, senator) => acc.seniority > senator.seniority ? acc : senator)
+//append stuff together here 
+loyaltyHeading.appendChild(cowardList)
 
-console.log(mostSeniorSenator)
-//And then we run the javaScript. 
-//senators is the huge array of data we're working with, then we simplify it and run it in our assembler function
-//(our html page has the script at the bottom)
-
-populateSenatorDiv(simplifiedSenators(senators))
+populateSenatorDiv(simplifiedMembers())
